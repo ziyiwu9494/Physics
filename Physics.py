a2bbf6074a8aad51f8b8a96dd0ab12533386814c
Ziyi <ziyiwu@usc.edu>
2020-06-22 02:38:25 -0700

    Add dipole support for turns

--- Physics.py
+++ Physics.py
@@ -13,6 +13,31 @@ class Point:
     def __init__(self, coords: list):
         self.coords = coords
 
+    def get_vector_to_b(self, b) -> np.ndarray:
+        """
+        return a vector pointing from current point to point b
+        :param b: Point b
+        :return: vector of a-b
+        """
+        return np.array(b.coords) - np.array(self.coords)
+
+    def find_electric_field(self, universe: list) -> np.ndarray:
+        """
+        find the electric field at point p
+        :param p: point p in n-dimensional coordinates
+        :param universe: all charged particles to be considered
+        note:must have same number of dimensions as p
+        :return: electric field vector at point p
+        """
+        e_vectors = np.empty(shape=(len(universe), len(self.coords)))
+        i = 0
+        for particle in universe:
+            r = particle.get_vector_to_b(self)
+            e_vector = COULOMB * particle.charge * r / (np.linalg.norm(r)) ** 3
+            e_vectors[i] = e_vector
+            i += 1
+        return np.sum(e_vectors, axis=0)
+
 
 class Particle(Point):
     """
@@ -25,29 +50,44 @@ class Particle(Point):
         self.mass = mass
 
 
-def get_vector_a_to_b(a: Point, b: Point) -> np.ndarray:
+class UEField:
     """
-    return a vector pointing from point a to point b
-    :param a: point a
-    :param b: point b
-    :return: vector of a-b
+    uniform electric field
     """
-    return np.array(b.coords) - np.array(a.coords)
+
+    def __init__(self,  vector: list, magnitude):
+        self.vector = vector
+        self.magnitude = magnitude
 
 
-def find_electric_field(p: Point, universe: list) -> np.ndarray:
+class Dipole:
     """
-    find the electric field at point p
-    :param p: point p in n-dimensional coordinates
-    :param universe: all charged particles to be considered
-    note:must have same number of dimensions as p
-    :return: electric field vector at point p
+    electric dipole
     """
-    e_vectors = np.empty(shape=(len(universe), len(p.coords)))
-    i = 0
-    for particle in universe:
-        r = get_vector_a_to_b(particle, p)
-        e_vector = COULOMB * particle.charge * r / (np.linalg.norm(r)) ** 3
-        e_vectors[i] = e_vector
-        i += 1
-    return np.sum(e_vectors, axis=0)
+
+    def __init__(self, mag_p, theta, mode="degrees"):
+        self.p = mag_p
+        self.mode = mode
+        self.theta = math.radians(theta) if mode == "degrees" else theta
+
+    def turn(self, theta: float, field: UEField) -> float:
+        """
+        turns dipole by theta degrees or radians and returns the amount of work done in the turn
+
+        :param theta: final angle
+        :param field: electric field
+        note: ignores direction
+        :return: work in joules
+        """
+        theta = math.radians(theta) if self.mode == "degrees" else theta
+        theta_i = self.theta
+        self.theta = theta
+        return self.p * field.magnitude * (math.cos(theta_i)-math.cos(theta_i+theta))
+
+
+
+
+
+
+
+
--- Test.py
+++ Test.py
@@ -3,8 +3,14 @@ import Physics
 
 def electric_field_test_1():
     env = [Physics.Particle([3, 0], float("3.20E-19"), 0), Physics.Particle([-3, 0], float("-3.20E-19"), 0)]
-    assert list(Physics.find_electric_field(Physics.Point([0, 3]), env)) == [-2.2627416997969528e-10, 0.0]
+    assert list(Physics.Point([0, 3]).find_electric_field(env)) == [-2.2627416997969528e-10, 0.0]
 
 
+def dipole_test_1():
+    field = Physics.UEField([], 34.9)
+    dipole = Physics.Dipole(float("1.97E-25"), 72.6)
+    assert dipole.turn(180, field) == float("4.111990317996545e-24")
 
 
+electric_field_test_1()
+dipole_test_1()
